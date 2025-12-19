bab1b014616ac0908bf32e2559c6a4b6129be11b
buraksenyurt <burakselimsenyurt@gmail.com>
2025-12-19 11:17:22 +0300

    Refactor game loop to manage game states

--- 1942/src/main.zig
+++ 1942/src/main.zig
@@ -3,12 +3,14 @@ const _1942 = @import("_1942");
 const rl = @import("raylib");
 const config = @import("config.zig").Config;
 
+var gameState: states = .Initial;
+
 pub fn main() !void {
     rl.initWindow(config.screenWidth, config.screenHeight, "1942 Game in Zig with Raylib");
     defer rl.closeWindow();
 
     rl.setTargetFPS(config.fps);
-    const titleText = rl.textFormat("1942 Game - Zig + Raylib", .{});
+    const titleText = rl.textFormat("1942 Game - Zig + Raylib\nPress ENTER to Start", .{});
     const sizeOfText: f32 = @floatFromInt(
         rl.measureText(titleText, config.titleFontSize),
     );
@@ -19,12 +21,44 @@ pub fn main() !void {
 
         rl.clearBackground(rl.Color.blue.alpha(0.5));
 
-        rl.drawText(
-            titleText,
-            @intFromFloat(config.screenWidth / 2 - sizeOfText / 2),
-            (config.screenHeight / 2) - config.titleFontSize,
-            config.titleFontSize,
-            rl.Color.white,
-        );
+        switch (gameState) {
+            .Initial => {
+                rl.drawText(
+                    titleText,
+                    @intFromFloat(config.screenWidth / 2 - sizeOfText / 2),
+                    (config.screenHeight / 2) - config.titleFontSize * 2,
+                    config.titleFontSize,
+                    rl.Color.white,
+                );
+
+                if (rl.isKeyPressed(rl.KeyboardKey.enter)) {
+                    gameState = .Playing;
+                }
+            },
+            .Playing => {
+                //todo@buraksenyurt: implement game update and draw logic
+
+                if (isGameOver()) {
+                    gameState = .Ended;
+                }
+            },
+            .Ended => {
+                rl.drawText("Game Over! Press R to Restart", 150, 400, 20, rl.Color.red);
+                if (rl.isKeyPressed(rl.KeyboardKey.r)) {
+                    gameState = .Initial;
+                }
+            },
+        }
     }
+}
+
+const states = enum {
+    Initial,
+    Playing,
+    Ended,
+};
+
+fn isGameOver() bool {
+    //todo@buraksenyurt: implement game over logic
+    return false;
 }
