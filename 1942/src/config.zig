ded040637276b6bb113f9ec0817f8b43a3e5ceaf
buraksenyurt <burakselimsenyurt@gmail.com>
2025-12-19 11:48:34 +0300

    Add player movement and drawing logic

--- 1942/src/config.zig
+++ 1942/src/config.zig
@@ -3,4 +3,7 @@ pub const Config = struct {
     pub const screenHeight: i32 = 800;
     pub const fps: i32 = 60;
     pub const titleFontSize: i32 = 30;
+    pub const playerSpeed: f32 = 7.0;
+    pub const playerWidth: f32 = 128.0;
+    pub const playerHeight: f32 = 128.0;
 };
--- 1942/src/main.zig
+++ 1942/src/main.zig
@@ -11,9 +11,18 @@ pub fn main() !void {
 
     rl.setTargetFPS(config.fps);
     const titleText = rl.textFormat("1942 Game - Zig + Raylib\nPress ENTER to Start", .{});
-    const sizeOfText: f32 = @floatFromInt(
+    const sizeOfTitleText: f32 = @floatFromInt(
         rl.measureText(titleText, config.titleFontSize),
     );
+    const gameOverText = "Game Over! Press R to Restart";
+    const sizeOfGameOverText: f32 = @floatFromInt(
+        rl.measureText(gameOverText, config.titleFontSize),
+    );
+
+    const playerTexture = try rl.loadTexture("resources/hero.png");
+    defer rl.unloadTexture(playerTexture);
+
+    var player = Player.init(playerTexture);
 
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
@@ -25,7 +34,7 @@ pub fn main() !void {
             .Initial => {
                 rl.drawText(
                     titleText,
-                    @intFromFloat(config.screenWidth / 2 - sizeOfText / 2),
+                    @intFromFloat(config.screenWidth / 2 - sizeOfTitleText / 2),
                     (config.screenHeight / 2) - config.titleFontSize * 2,
                     config.titleFontSize,
                     rl.Color.white,
@@ -36,14 +45,21 @@ pub fn main() !void {
                 }
             },
             .Playing => {
-                //todo@buraksenyurt: implement game update and draw logic
+                player.update();
+                player.draw();
 
                 if (isGameOver()) {
                     gameState = .Ended;
                 }
             },
             .Ended => {
-                rl.drawText("Game Over! Press R to Restart", 150, 400, 20, rl.Color.red);
+                rl.drawText(
+                    gameOverText,
+                    @intFromFloat(config.screenWidth / 2 - sizeOfGameOverText / 2),
+                    (config.screenHeight / 2) - config.titleFontSize * 2,
+                    config.titleFontSize,
+                    rl.Color.red,
+                );
                 if (rl.isKeyPressed(rl.KeyboardKey.r)) {
                     gameState = .Initial;
                 }
@@ -62,3 +78,68 @@ fn isGameOver() bool {
     //todo@buraksenyurt: implement game over logic
     return false;
 }
+
+const Player = struct {
+    position: rl.Vector2,
+    size: rl.Vector2,
+    asset: rl.Texture2D,
+
+    pub fn init(texture: rl.Texture2D) @This() {
+        return .{
+            .position = rl.Vector2{
+                .x = config.screenWidth / 2 - config.playerWidth / 2,
+                .y = config.screenHeight - config.playerHeight,
+            },
+            .size = rl.Vector2{
+                .x = config.playerWidth,
+                .y = config.playerHeight,
+            },
+            .asset = texture,
+        };
+    }
+
+    pub fn update(self: *@This()) void {
+        if (rl.isKeyDown(rl.KeyboardKey.right)) {
+            self.position.x += config.playerSpeed;
+            if (self.position.x > @as(f32, @floatFromInt(config.screenWidth)) - self.size.x) {
+                self.position.x = @as(f32, @floatFromInt(config.screenWidth)) - self.size.x;
+            }
+        }
+        if (rl.isKeyDown(rl.KeyboardKey.left)) {
+            self.position.x -= config.playerSpeed;
+            if (self.position.x < 0) {
+                self.position.x = 0;
+            }
+        }
+        if (rl.isKeyDown(rl.KeyboardKey.up)) {
+            self.position.y -= config.playerSpeed;
+            if (self.position.y < 0) {
+                self.position.y = 0;
+            }
+        }
+        if (rl.isKeyDown(rl.KeyboardKey.down)) {
+            self.position.y += config.playerSpeed;
+            if (self.position.y > @as(f32, @floatFromInt(config.screenHeight)) - self.size.y) {
+                self.position.y = @as(f32, @floatFromInt(config.screenHeight)) - self.size.y;
+            }
+        }
+    }
+
+    pub fn draw(self: @This()) void {
+        rl.drawTexture(
+            self.asset,
+            @intFromFloat(self.position.x),
+            @intFromFloat(self.position.y),
+            rl.Color.white,
+        );
+    }
+
+    pub fn getRectangle(self: *const Player) rl.Rectangle {
+        return rl.Rectangle{
+            .x = self.position.x,
+            .y = self.position.y,
+            .width = self.size.x,
+            .height = self.size.y,
+        };
+    }
+};
